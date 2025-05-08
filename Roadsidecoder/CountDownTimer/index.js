0e2516660a3f82bc36cc18861ae7e80a79dd38e0
vtarun <vivektarun89@gmail.com>
2025-05-08 22:54:30 +0530

    Initial setup

---
+++ Roadsidecoder/CountDownTimer/index.js
@@ -0,0 +1,8 @@
+const startCounter = document.getElementById('startCounter');
+const resetCounter = document.getElementById('resetCounter');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+
+
