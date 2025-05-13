ccdfefbf58988f613e3f7dfedb6be0e67185bcf9
vtarun <vivektarun89@gmail.com>
2025-05-13 23:29:27 +0530

    Update counter method

--- Roadsidecoder/CountDownTimer/index.js
+++ Roadsidecoder/CountDownTimer/index.js
@@ -1,8 +1,39 @@
-const startCounter = document.getElementById('startCounter');
-const resetCounter = document.getElementById('resetCounter');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
+(function (){
+  const startCounterBtn = document.getElementById('startCounter');
+  const resetCounterBtn = document.getElementById('resetCounter');
+  const hours = document.querySelector('.hours');
+  const minutes = document.querySelector('.minutes');
+  const seconds = document.querySelector('.seconds');
+
+  let counterInterval = null;
+
+  function resetCounter() {
+    hours.value = '';
+    minutes.value = '';
+    seconds.value = '';
+    clearInterval(counterInterval);
+  }
+
+  function startCounter() {
+    if(counterInterval) return;
+
+    counterInterval = setInterval(function(){
+        hours.value -= 1;
+    }, 1000);
+  }
+
+  startCounterBtn.addEventListener('click', function() {
+    startCounter();
+  });
+
+  resetCounterBtn.addEventListener('click', function() {
+    resetCounter();
+  });
+  
+})()
+
+
+
 
 
 
