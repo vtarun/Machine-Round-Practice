7b5ac85ab4a0128c3110f9f06d77aeb947794315
vtarun <vivektarun89@gmail.com>
2025-05-14 13:48:35 +0530

    Finish counter timer

--- Roadsidecoder/CountDownTimer/index.js
+++ Roadsidecoder/CountDownTimer/index.js
@@ -1,9 +1,10 @@
 (function (){
   const startCounterBtn = document.getElementById('startCounter');
   const resetCounterBtn = document.getElementById('resetCounter');
-  const hours = document.querySelector('.hours');
-  const minutes = document.querySelector('.minutes');
-  const seconds = document.querySelector('.seconds');
+  const stopCounterBtn = document.getElementById('stopCounter');
+  const hours = document.querySelector('.timer__input--hours');
+  const minutes = document.querySelector('.timer__input--minutes');
+  const seconds = document.querySelector('.timer__input--seconds');
 
   let counterInterval = null;
 
@@ -14,22 +15,63 @@
     clearInterval(counterInterval);
   }
 
+  function adjustCounter() {
+    if(seconds.value > 60){
+      minutes.value += 1;
+      seconds.value = parseInt(seconds.value) - 60;
+    }
+
+    if(minutes.value > 60){
+      hours.value += 1;
+      seconds.value = parseInt(minutes.value) - 60;
+    }
+  }
+
+  function timer(){
+    if(seconds.value != 0){
+        seconds.value = `${seconds.value > 10  ? '' : '0'}${seconds.value - 1}`
+      }
+      else if(minutes.value != 0 && seconds.value == 0){
+        seconds.value = 59;
+        minutes.value = `${minutes.value > 10  ? '' : '0'}${minutes.value - 1}`
+      }
+      else if(hours.value != 0 && minutes.value == 0){
+        hours.value = `${hours.value > 10  ? '' : '0'}${hours.value - 1}`
+        minutes.value = 60;
+      }
+  }
+
   function startCounter() {
-    if(counterInterval) return;
+    
+    if(hours.value == 0 && minutes.value == 0 && seconds.value == 0){
+        resetCounter();
+        return;
+    }
+
+    startCounterBtn.style.display = "none";
+    stopCounterBtn.style.display = "initial";
 
     counterInterval = setInterval(function(){
-        hours.value -= 1;
+      timer();
     }, 1000);
+
   }
 
   startCounterBtn.addEventListener('click', function() {
+    adjustCounter();
     startCounter();
+  });
+
+  stopCounterBtn.addEventListener('click', function() {
+    stopCounterBtn.style.display = "none";
+    startCounterBtn.style.display = "initial";
+    clearInterval(counterInterval);
   });
 
   resetCounterBtn.addEventListener('click', function() {
     resetCounter();
   });
-  
+
 })()
 
 
