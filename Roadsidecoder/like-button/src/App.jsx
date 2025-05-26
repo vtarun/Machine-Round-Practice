f00bab86649de7e04af59720f4be4b3ba64244c5
vtarun <vivektarun89@gmail.com>
2025-05-26 23:37:54 +0530

    Complete implementation

--- Roadsidecoder/like-button/src/App.jsx
+++ Roadsidecoder/like-button/src/App.jsx
@@ -6,30 +6,44 @@ const url = "https://questions.greatfrontend.com/api/questions/like-button";
 export default function App() {
   const [like, setLike] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
   const handleClick = async () => {
-    setButtonState(prev => like ? 'likeLoading' : 'unlikeLoading');
-    const payload = like ? 'like' : 'unlike';
+    setLoading(true);
+    setError('');
+    const payload = { action : like ? 'like' : 'unlike' };
     try {
-      const response = await fetch(url, (method = "POST"), { action: payload });
-      const result = response.json();
-      if(result.message == 'sucess'){
-        setLike(true);
+      const response = await fetch(url, {
+        method : "POST", 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload) 
+      });
+
+      const result = await response.json();
+      if(result.message === 'Success!'){       
+        setLike(prev => !prev);
+      }else{
+        throw new Error(result.message)
       }
-    } catch (error) {
-
+    } catch (err) {
+      setError(err.message);
     }finally{
-
+      setLoading(false);
     }
   };
+
 
   return (
     <div>
       {
-        !loading && (<button onClick={handleClick} className={`like ? 'like' : 'default'`}><HeartIcon /> Like </button>)
+        !loading && (<button onClick={handleClick} className={like ? 'like' : 'default'}><HeartIcon /> Like </button>)
       }
       {
-        loading && ( <button onClick={handleClick} className={`like ? 'likeLoading' : 'defaultLoading'`}><SpinnerIcon /> Like </button>)
+        loading && ( <button onClick={handleClick} className={like ? 'likeLoading' : 'defaultLoading'}><SpinnerIcon /> Like </button>)
       }
+      {error && <p>{error}</p>}
     </div>
   );
 }
