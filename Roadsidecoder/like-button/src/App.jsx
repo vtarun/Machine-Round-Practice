1514ed7bb6e6698e9b1e5eb13e2f1bb35d81cdad
vtarun <vivektarun89@gmail.com>
2025-05-29 22:40:57 +0530

    Finished liked button

--- Roadsidecoder/like-button/src/App.jsx
+++ Roadsidecoder/like-button/src/App.jsx
@@ -1,36 +1,38 @@
 import { HeartIcon, SpinnerIcon } from "./icons.jsx";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const url = "https://questions.greatfrontend.com/api/questions/like-button";
 
 export default function App() {
-  const [like, setLike] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [liked, setLiked] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
 
-  const handleClick = async () => {
-    setLoading(true);
-    setError('');
-    const payload = { action : like ? 'like' : 'unlike' };
+  const handleLikeUnlike = async () => {
+    setError(null);
+    setIsFetching(true);
+
     try {
       const response = await fetch(url, {
         method : "POST", 
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(payload) 
+        body: JSON.stringify({action: liked ? 'unlike': 'like'}) 
       });
 
-      const result = await response.json();
-      if(result.message === 'Success!'){       
-        setLike(prev => !prev);
+      
+      if(response.status >= 200 && response.status < 300){       
+        setLiked(!liked);
       }else{
-        throw new Error(result.message)
+        const result = await response.json();
+        setError(result.message);
+        return
       }
     } catch (err) {
       setError(err.message);
     }finally{
-      setLoading(false);
+      setIsFetching(false);
     }
   };
 
@@ -38,10 +40,10 @@ export default function App() {
   return (
     <div>
       {
-        !loading && (<button onClick={handleClick} className={like ? 'like' : 'default'}><HeartIcon /> Like </button>)
+        !isFetching  && (<button onClick={handleLikeUnlike} className={liked ? 'like' : 'default'}><HeartIcon /> Like </button>)
       }
       {
-        loading && ( <button onClick={handleClick} className={like ? 'likeLoading' : 'defaultLoading'}><SpinnerIcon /> Like </button>)
+        isFetching  && ( <button onClick={handleLikeUnlike} className={liked ? 'likeLoading' : 'defaultLoading'}><SpinnerIcon /> Like </button>)
       }
       {error && <p>{error}</p>}
     </div>
